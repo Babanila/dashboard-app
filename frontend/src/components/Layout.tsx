1dcd2c42691d4ad3e848d302f6413521074c0038
Babajide Williams <babanila@yahoo.com>
2025-05-30 23:43:57 +0200

    feat(): Add product component

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router';
 
 const Layout: React.FC = () => {
   return (
-    <main className="mt-[65px] w-full h-full min-h-[80vh] flex flex-col bg-primary py-8">
+    <main className="mt-[65px] w-full h-full min-h-[80vh] flex flex-col justify-center items-center bg-primary py-8">
       <Outlet />
     </main>
   );
--- frontend/src/pages/Product.tsx
+++ frontend/src/pages/Product.tsx
@@ -1,7 +1,116 @@
-import React from 'react';
+import React, { FC, useState } from 'react';
+import { useNavigate, useParams } from 'react-router';
+import { useProductDetails } from '@/hooks/useProduct';
+import Button from '@/components/Button';
+import LoadingSpinner from '@/components/LoadingSpinner';
+import { ProductDetailsProps } from '@/types';
 
 const Product: React.FC = () => {
-  return <div className="mt-[65px] w-full h-full min-h-[50vh] flex flex-col bg-gray-100">Product Page</div>;
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const { isLoading, error, productDetails } = useProductDetails(id);
+
+  const handleBack = () => {
+    navigate('/');
+  };
+
+  return (
+    <div className="h-full flex flex-col justify-center items-center space-y-8 bg-transparent">
+      {productDetails && <ProductCard product={productDetails} />}
+
+      {isLoading && (
+        <div className="flex justify-center">
+          <LoadingSpinner size="w-15 h-15" />
+        </div>
+      )}
+
+      {error && !isLoading && (
+        <p className="flex justify-center text-bred">Error loading product details</p>
+      )}
+
+      <Button
+        className="w-32 bg-secondary text-primary font-semibold px-6 py-3 rounded-lg hover:bg-bgreen transition cursor-pointer"
+        onClick={handleBack}
+        children="Back"
+      />
+    </div>
+  );
 };
 
 export default Product;
+
+export const ProductCard: FC<{ product: ProductDetailsProps }> = ({ product }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const images = product.images;
+  const imagesLength = product.images.length > 1;
+
+  const prevImage = () => {
+    setCurrentImageIndex((prev) => (prev === 0 ? images.length - 1 : prev - 1));
+  };
+
+  const nextImage = () => {
+    setCurrentImageIndex((prev) => (prev === images.length - 1 ? 0 : prev + 1));
+  };
+
+  return (
+    <div className="w-full max-w-5xl mx-auto bg-secondary rounded-2xl shadow-lg overflow-hidden border border-light-gray2 p-6 my-6 flex flex-col items-center justify-center md:flex-row md:gap-6">
+      <div className="relative w-full md:w-1/2 flex items-center justify-center">
+        <img
+          className="object-contain h-80 w-full rounded-md"
+          src={images[currentImageIndex]}
+          alt={product.title}
+        />
+        {imagesLength && (
+          <>
+            <Button
+              onClick={prevImage}
+              className="absolute left-2 top-1/2 transform -translate-y-1/2 bg-primary text-secondary hover:bg-bgreen p-2 rounded-full"
+              children="&lt;"
+            />
+            <Button
+              onClick={nextImage}
+              className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-primary text-secondary hover:bg-bgreen p-2 rounded-full"
+              children="&gt;"
+            />
+          </>
+        )}
+      </div>
+
+      <div className="mt-4 md:mt-0 md:w-1/2 flex flex-col gap-4">
+        <h1 className="text-2xl font-bold text-primary">{product.title}</h1>
+        <p className="text-sm text-light-gray5">By {product.brand}</p>
+        <p className="text-base text-primary-one leading-relaxed">{product.description}</p>
+
+        <div className="text-lg text-primary font-medium">
+          ${product.price}{' '}
+          <span className="text-bgreen font-semibold">({product.discountPercentage}% off)</span>
+        </div>
+
+        <div className="text-sm space-y-1 text-light-gray5">
+          <p>
+            <strong>Availability:</strong>{' '}
+            <span className="text-bgreen font-medium">{product.availabilityStatus}</span>
+          </p>
+          <p>
+            <strong>SKU:</strong> {product.sku}
+          </p>
+          <p>
+            <strong>Rating:</strong> {product.rating} / 5
+          </p>
+        </div>
+
+        <div className="text-sm space-y-1 text-light-gray5">
+          <p>
+            <strong>Shipping:</strong> {product.shippingInformation}
+          </p>
+          <p>
+            <strong>Warranty:</strong> {product.warrantyInformation}
+          </p>
+          <p>
+            <strong>Return Policy:</strong> {product.returnPolicy}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
