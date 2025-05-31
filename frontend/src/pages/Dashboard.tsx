404683d61053458faa0f523fd478f60d52c1beea
Babajide Williams <babanila@yahoo.com>
2025-05-31 10:29:05 +0200

    feat(): Update dashboard page

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
+import Button from '@/components/Button';
+import FeaturedProducts from '@/components/FeaturedProducts';
+import ServiceOffers from '@/components/ServiceOffers';
+import SubscriptionForm from '@/components/SubcriptionForm';
 
 const Dashboard: React.FC = () => {
-  return <div className="w-full flex h-auto bg-gray-100">Dashboard Page</div>;
+  return (
+    <div className="flex flex-col">
+      <WelcomeSection />
+      <ServiceOffers />
+      <FeaturedProducts />
+      <SubscriptionForm />
+    </div>
+  );
 };
 
 export default Dashboard;
+
+const WelcomeSection = () => {
+  let navigate = useNavigate();
+
+  const handleGoToProducts = () => {
+    navigate('/products');
+  };
+  return (
+    <section className="bg-primary text-secondary py-16 px-6 text-center">
+      <h1 className="text-4xl md:text-6xl font-bold mb-4">Welcome to DASH</h1>
+      <p className="text-lg md:text-xl mb-8">
+        Your one-stop shop for premium tech, fashion, and more.
+      </p>
+      <Button
+        className="bg-secondary text-primary font-semibold px-6 py-3 rounded-lg hover:bg-bgreen transition cursor-pointer"
+        onClick={handleGoToProducts}
+        children="Shop Now"
+      />
+    </section>
+  );
+};
