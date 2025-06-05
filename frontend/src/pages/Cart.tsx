c4da25d7cd8b4121e0f7d0f0f637b5a36d8713e0
Babajide Williams <babanila@yahoo.com>
2025-06-05 12:07:28 +0200

    feat(): Add cart page

---
+++ frontend/src/pages/Cart.tsx
@@ -0,0 +1,11 @@
+import type { FC } from "react";
+
+const Cart: FC = () => {
+	return (
+		<div className="h-full flex flex-col justify-center items-center space-y-8 bg-primary">
+			This is the page for cart where product in the basket are shown
+		</div>
+	);
+};
+
+export default Cart;
